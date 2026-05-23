05e9d8f6446fda9bf2eedd4f624b8332d842d3cb
RGitLog18 <rajeedandge444@gmail.com>
2026-05-23 20:58:06 +0530

    updatefinal89

--- Backend/takeimage.py
+++ Backend/takeimage.py
@@ -49,7 +49,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173", 
         "http://127.0.0.1:5173",
-        "https://nivaran-lbg3.onrender.com"
+        "https://nivaran-ai.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
